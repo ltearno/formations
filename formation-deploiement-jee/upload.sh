f48b5cbd010809e60782972dea9db3cc9d85b93a
Arnaud Tournier <arnaud.tournier@stack-labs.com>
2018-05-25 11:05:58 +0200

    modifs

--- formation-deploiement-jee/upload.sh
+++ formation-deploiement-jee/upload.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
 
-scp *.html arnaud@lteconsulting.fr:/var/www/lteconsulting.mano/javaee-deploiement/
+scp *.html arnaud@lteconsulting.fr:/var/www/lteconsulting.mano/formations/deploiement-jee/
