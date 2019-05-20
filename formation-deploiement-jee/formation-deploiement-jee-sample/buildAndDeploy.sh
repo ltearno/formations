d14be8c2320a72f58cd0c9a1aa8b88105fa47dc0
Arnaud Tournier <arnaud.tournier@stack-labs.com>
2019-05-20 22:20:19 +0200

    wip

--- formation-deploiement-jee/formation-deploiement-jee-sample/buildAndDeploy.sh
+++ formation-deploiement-jee/formation-deploiement-jee-sample/buildAndDeploy.sh
@@ -2,36 +2,36 @@
 
 # application
 mvn clean install
-sudo docker build -t ltearno/application .
+docker build -t ltearno/application .
 
 # base de donnees sessions
-sudo docker build -t ltearno/mariadb -f session-database.dockerfile .
+docker build -t ltearno/mariadb -f session-database.dockerfile .
 
 # load balancer Nginx
-sudo docker build -t ltearno/lb -f nginx-loadbalancer.dockerfile .
+docker build -t ltearno/lb -f nginx-loadbalancer.dockerfile .
 
 # arret des containers
 echo "Arret des containers"
-sudo docker rm -f load_balancer
+docker rm -f load_balancer
 for i in {0..3}
 do
-	sudo docker rm -f application0$i
+	docker rm -f application0$i
 done
-sudo docker rm -f database
+docker rm -f database
 
 # lancement base de données
 echo "Lancement base de données"
-sudo docker run -d --name database ltearno/mariadb
+docker run -d --name database ltearno/mariadb
 
 # lancement serveurs d'application
 for i in {0..3}
 do
 	echo "Lancement server d'application $i"
-	sudo docker run -d --name application0$i --link database:database ltearno/application
+	docker run -d --name application0$i --link database:database ltearno/application
 done
 
 # lancement du load balancer
-cmd='sudo docker run -d --name load_balancer '
+cmd='docker run -d --name load_balancer '
 for i in {0..3}
 do
 	cmd="$cmd --link application0$i:application0$i"
@@ -41,4 +41,4 @@ echo "Lancement du load balancer"
 eval $cmd
 
 echo "Liste de containers lancés"
-sudo docker ps -a
+docker ps -a
