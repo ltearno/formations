0e493f6e345a6cc25625e605b4695b21051e7c10
Arnaud Tournier <ltearno@gmail.com>
2020-05-18 13:13:30 +0200

    improvements

--- formation-deploiement-jee/formation-deploiement-jee-sample/squelette-tp/src/main/java/fr/lteconsulting/Tools.java
+++ formation-deploiement-jee/formation-deploiement-jee-sample/squelette-tp/src/main/java/fr/lteconsulting/Tools.java
@@ -3,5 +3,8 @@ package fr.lteconsulting;
 import javax.servlet.http.Cookie;
 import javax.servlet.http.HttpServletRequest;
 
+/*
+    Utilisez cette classe à votre convenance, vous pouvez même la supprimer !
+*/
 public class Tools {
 }
